cd18d8416c44e7fe8451ae143fa965d69279bdf4
People-11 <349504120p@gmail.com>
2025-01-12 08:16:40 +1100

    chore: Remove share button in Channel

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/ChatMessageCell.java
@@ -16192,7 +16192,8 @@ public class ChatMessageCell extends BaseCell implements SeekBar.SeekBarDelegate
                 return false;
             }
         }
-        return messageObject.needDrawShareButton();
+        //return messageObject.needDrawShareButton();
+        return false;
     }
 
     public boolean isInsideBackground(float x, float y) {
