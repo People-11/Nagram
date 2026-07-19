c3bc9e5ac39c6d885c755c0cbe60214c5ce35b18
People-11 <41558215+People-11@users.noreply.github.com>
2026-07-19 16:04:25 +0930

    Force disable update check

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/remote/UpdateHelper.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/helpers/remote/UpdateHelper.java
@@ -1,19 +1,8 @@
 package tw.nekomimi.nekogram.helpers.remote;
 
-import android.os.Build;
-
-import org.json.JSONException;
 import org.json.JSONObject;
-import org.telegram.messenger.BuildConfig;
-import org.telegram.tgnet.TLObject;
-import org.telegram.tgnet.TLRPC;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import tw.nekomimi.nekogram.NekoXConfig;
 
 public class UpdateHelper extends BaseRemoteHelper {
     private static final class InstanceHolder {
@@ -24,145 +13,33 @@ public class UpdateHelper extends BaseRemoteHelper {
         return InstanceHolder.instance;
     }
 
-    private boolean updateAlways = false;
-
     @Override
     protected void onError(String text, Delegate delegate) {
-        delegate.onTLResponse(null, text);
+        if (delegate != null) {
+            delegate.onTLResponse(null, text);
+        }
     }
 
     @Override
     protected String getTag() {
-        return NekoXConfig.autoUpdateReleaseChannel >= 2 ? "updatetest" : "updatev2";
-    }
-
-    @SuppressWarnings("ConstantConditions")
-    private int getPreferredAbiFile(Map<String, Integer> files) {
-        for (String abi : Build.SUPPORTED_ABIS) {
-            if (files.containsKey(abi)) {
-                return files.get(abi);
-            }
-        }
-        return files.get("arm64-v8a");
-    }
-
-    private Map<String, Integer> jsonToMap(JSONObject obj) {
-        Map<String, Integer> map = new HashMap<>();
-        try {
-            map.put("arm64-v8a", obj.getInt("arm64-v8a"));
-        } catch (JSONException ignored) {}
-        return map;
-    }
-
-    private Update getShouldUpdateVersion(List<JSONObject> responses) {
-        long currentVersion = BuildConfig.VERSION_CODE;
-        long currentTimestamp = BuildConfig.BUILD_TIMESTAMP;
-        Update ref = null;
-        for (var string : responses) {
-            try {
-                int versionCode = string.getInt("version_code");
-                long timestamp = string.getLong("timestamp");
-                if (versionCode > currentVersion
-                        || (versionCode == currentVersion && timestamp > currentTimestamp)
-                        || updateAlways) {
-                    if (updateAlways) {
-                        updateAlways = false;
-                    }
-                    ref = new Update(
-                            string.getBoolean("can_not_skip"),
-                            string.getString("version"),
-                            versionCode,
-                            timestamp,
-                            string.getInt("sticker"),
-                            string.getInt("message"),
-                            jsonToMap(string.getJSONObject("gcm")),
-                            string.getString("url")
-                    );
-                    break;
-                }
-            } catch (JSONException ignored) {}
-        }
-        return ref;
-    }
-
-    private void getNewVersionMessagesCallback(Delegate delegate, Update json,
-                                               HashMap<String, Integer> ids, TLObject response) {
-        var update = new TLRPC.TL_help_appUpdate();
-        update.version = json.version;
-        update.can_not_skip = json.canNotSkip;
-        if (json.url != null) {
-            update.url = json.url;
-            update.flags |= 4;
-        }
-        if (NekoXConfig.autoUpdateReleaseChannel == 0 && !update.can_not_skip) {
-            delegate.onTLResponse(null, null);
-            return;
-        }
-        if (response != null) {
-            var res = (TLRPC.messages_Messages) response;
-            getMessagesController().removeDeletedMessagesFromArray(CHANNEL_METADATA_ID, res.messages);
-            var messages = new HashMap<Integer, TLRPC.Message>();
-            for (var message : res.messages) {
-                messages.put(message.id, message);
-            }
-
-            if (ids.containsKey("file")) {
-                var file = messages.get(ids.get("file"));
-                if (file != null && file.media != null) {
-                    update.document = file.media.document;
-                    update.flags |= 2;
-                }
-            }
-            if (ids.containsKey("message")) {
-                var message = messages.get(ids.get("message"));
-                if (message != null) {
-                    update.text = message.message;
-                    update.entities = message.entities;
-                }
-            }
-            if (ids.containsKey("sticker")) {
-                var sticker = messages.get(ids.get("sticker"));
-                if (sticker != null && sticker.media != null) {
-                    update.sticker = sticker.media.document;
-                    update.flags |= 8;
-                }
-            }
-        }
-        delegate.onTLResponse(update, null);
+        return "update-disabled";
     }
 
     @Override
     protected void onLoadSuccess(ArrayList<JSONObject> responses, Delegate delegate) {
-        var update = getShouldUpdateVersion(responses);
-        if (update == null) {
+        if (delegate != null) {
             delegate.onTLResponse(null, null);
-            return;
         }
+    }
 
-        var ids = new HashMap<String, Integer>();
-        if (update.message != null) {
-            ids.put("message", update.message);
-        }
-        if (update.sticker != null) {
-            ids.put("sticker", update.sticker);
-        }
-        if (update.gcm != null) {
-            ids.put("file", getPreferredAbiFile(update.gcm));
-        }
+    @Override
+    public void load() {
+    }
 
-        if (ids.isEmpty()) {
-            getNewVersionMessagesCallback(delegate, update, null, null);
-        } else {
-            var req = new TLRPC.TL_channels_getMessages();
-            req.channel = getMessagesController().getInputChannel(CHANNEL_METADATA_ID);
-            req.id = new ArrayList<>(ids.values());
-            getConnectionsManager().sendRequest(req, (response1, error1) -> {
-                if (error1 == null) {
-                    getNewVersionMessagesCallback(delegate, update, ids, response1);
-                } else {
-                    delegate.onTLResponse(null, error1.text);
-                }
-            });
+    @Override
+    public void load(Delegate delegate) {
+        if (delegate != null) {
+            delegate.onTLResponse(null, null);
         }
     }
 
@@ -170,30 +47,9 @@ public class UpdateHelper extends BaseRemoteHelper {
         checkNewVersionAvailable(delegate, false);
     }
 
-    public void checkNewVersionAvailable(Delegate delegate, boolean updateAlways_) {
-        updateAlways = updateAlways_;
-        load(delegate);
-    }
-
-    public static class Update {
-        public Boolean canNotSkip;
-        public String version;
-        public Integer versionCode;
-        public Long timeStamp;
-        public Integer sticker;
-        public Integer message;
-        public Map<String, Integer> gcm;
-        public String url;
-
-        public Update(Boolean canNotSkip, String version, int versionCode, long timeStamp, int sticker, int message, Map<String, Integer> gcm, String url) {
-            this.canNotSkip = canNotSkip;
-            this.version = version;
-            this.versionCode = versionCode;
-            this.timeStamp = timeStamp;
-            this.sticker = sticker;
-            this.message = message;
-            this.gcm = gcm;
-            this.url = url;
+    public void checkNewVersionAvailable(Delegate delegate, boolean updateAlways) {
+        if (delegate != null) {
+            delegate.onTLResponse(null, null);
         }
     }
 }
