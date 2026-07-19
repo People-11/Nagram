9dc80c8d05ae3ec834a83c064ee33dd119dd1b0c
People-11 <41558215+People-11@users.noreply.github.com>
2026-07-19 16:17:56 +0930

    Don‘t force 512x512 when sticker untouched

--- TMessagesProj/src/main/java/org/telegram/ui/Components/Paint/Views/StickerMakerView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/Paint/Views/StickerMakerView.java
@@ -101,6 +101,7 @@ public class StickerMakerView extends FrameLayout implements NotificationCenter.
     private SegmentedObject selectedObject;
     public float outlineWidth = 2f;
     public boolean empty;
+    public boolean showFrameBorder = false;
     public SegmentedObject[] objects;
     private volatile Bitmap sourceBitmap;
     public int orientation;
@@ -779,8 +780,10 @@ public class StickerMakerView extends FrameLayout implements NotificationCenter.
     @Override
     protected void dispatchDraw(Canvas canvas) {
         super.dispatchDraw(canvas);
-        canvas.drawPath(screenPath, bgPaint);
-        canvas.drawPath(dashPath, dashPaint);
+        if (showFrameBorder) {
+            canvas.drawPath(screenPath, bgPaint);
+            canvas.drawPath(dashPath, dashPaint);
+        }
     }
 
     private Bitmap createSmoothEdgesSegmentedImage(int x, int y, Bitmap inputBitmap, boolean full) {
