86dcffe789d5eba69be5a9112d62719f5ac00443
People-11 <41558215+People-11@users.noreply.github.com>
2026-08-05 19:11:40 +0930

    Fix crash when "Remove Animation" enabled in system

--- TMessagesProj/src/main/java/org/telegram/ui/MainTabsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/MainTabsActivity.java
@@ -874,10 +874,15 @@ public class MainTabsActivity extends ViewPagerActivity implements NotificationC
     }
 
     public void setGestureSelectedOverride(float animatedPosition, boolean allow) {
+        if (tabs == null || tabsView == null) {
+            return;
+        }
         for (int index = 0; index < tabs.length; index++) {
-            final int position = indexToPosition(index);
-            final float visibility = Math.max(0, 1f - Math.abs(position - animatedPosition));
-            tabs[index].setGestureSelectedOverride(visibility, allow);
+            if (tabs[index] != null) {
+                final int position = indexToPosition(index);
+                final float visibility = Math.max(0, 1f - Math.abs(position - animatedPosition));
+                tabs[index].setGestureSelectedOverride(visibility, allow);
+            }
         }
         tabsView.invalidate();
     }
@@ -928,6 +933,9 @@ public class MainTabsActivity extends ViewPagerActivity implements NotificationC
     @NonNull
     @Override
     protected WindowInsetsCompat onApplyWindowInsets(@NonNull View v, @NonNull WindowInsetsCompat insets) {
+        if (updateLayoutWrapper == null || fadeView == null || viewPager == null || tabsViewWrapper == null) {
+            return super.onApplyWindowInsets(v, insets);
+        }
         navigationBarHeight = insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom;
         final boolean isUpdateLayoutVisible = updateLayoutWrapper.isUpdateLayoutVisible();
         final int updateLayoutHeight = isUpdateLayoutVisible ? dp(UpdateLayoutWrapper.HEIGHT) : 0;
@@ -1091,6 +1099,9 @@ public class MainTabsActivity extends ViewPagerActivity implements NotificationC
     }
 
     private void checkUi_tabsPosition() {
+        if (updateLayoutWrapper == null || tabsViewWrapper == null || tabsView == null) {
+            return;
+        }
         final boolean isUpdateLayoutVisible = updateLayoutWrapper.isUpdateLayoutVisible();
         final int updateLayoutHeight = isUpdateLayoutVisible ? dp(UpdateLayoutWrapper.HEIGHT) : 0;
         final int normalY = -(updateLayoutHeight);
